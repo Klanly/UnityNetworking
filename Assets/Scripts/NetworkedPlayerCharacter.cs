df6a5b24b078e793a5bcb06f00175318d7497a13
Justin Wilkinson <wilkinsense@gmail.com>
2015-12-17 23:12:55 -0500

    Initial commit, featuring most of the work I've already done to populate the Test scene with players.

---
+++ Assets/Scripts/NetworkedPlayerCharacter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.Networking;
+
+public class NetworkedPlayerCharacter : ThirdPersonCharacter {
+  NetworkIdentity identity;
+
+  public override void Start() {
+    base.Start();
+
+    if(identity == null) {
+      identity = GetComponent<NetworkIdentity>();
+    }
+  }
+
+  public override void Move(Vector3 move, bool crouch, bool jump) {
+    if(identity.isLocalPlayer) {
+      base.Move(move, crouch, jump);
+    }
+  }
+}
